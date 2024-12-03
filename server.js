f1546ccb206f9789bd3d5698b9fa2d40791aed1d
Souvik Ghorai <souvikghorai269@gmail.com>
2024-12-03 12:23:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5001;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+.connect(
+    "mongodb+srv://souvikghorai269:souvik1234@cluster0.xjec7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {})
+
+.then(()=>{
+    console.log("Connected to Database");
+});
+
+//routes
+const userRoute = require("./routes/user.route");
+const todoRoute=require("./routes/todo.route");
+app.use("/user", userRoute);
+app.use("/todo",todoRoute);
+app.get("/", (req, res) => {
+    res.send("Hello World");
+    res.end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
